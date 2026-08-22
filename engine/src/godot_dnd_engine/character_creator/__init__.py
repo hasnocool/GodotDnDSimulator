ab2c3f04378c3857a01779734987851707294fec
@fleshwounded <101779643+hasnocool@users.noreply.github.com>
2026-08-22 16:00:15 -0700

    feat(character): export v0.9 creator API

---
+++ engine/src/godot_dnd_engine/character_creator/__init__.py
@@ -0,0 +1,31 @@
+# engine/src/godot_dnd_engine/character_creator/__init__.py
+"""Rules-driven v0.9 character creator and advancement API."""
+
+from .catalog import demo_character_catalog
+from .model import (
+    AbilityScorePolicy,
+    CharacterCreatorCatalog,
+    CharacterDraft,
+    CharacterRecord,
+    CreationChoice,
+    CreationGroup,
+    CreationStep,
+    InventoryGrant,
+)
+from .runtime import CharacterCreatorRuntime
+from .service import CharacterCreatorService, record_to_dict
+
+__all__ = [
+    "AbilityScorePolicy",
+    "CharacterCreatorCatalog",
+    "CharacterCreatorRuntime",
+    "CharacterCreatorService",
+    "CharacterDraft",
+    "CharacterRecord",
+    "CreationChoice",
+    "CreationGroup",
+    "CreationStep",
+    "InventoryGrant",
+    "demo_character_catalog",
+    "record_to_dict",
+]
